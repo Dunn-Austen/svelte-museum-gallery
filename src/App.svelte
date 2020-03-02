836540264ec95e4343db21f802ebb3fd702f7d0a
Alan Turing <austeyd@gmail.com>
2020-03-02 12:24:26 -0700

    Add scaffolding for initial fetch attempt

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,17 @@
 <script>
 	export let name;
+	import { onMount } from 'svelte';
+
+let photos = [];
+
+onMount(async () => {
+	const res = await fetch(`https://api.harvardartmuseums.org/RESOURCE_TYPE?apikey=Y27ab6e80-5caf-11ea-8c5a-cb60061dc4a9?size=100&page=5`);
+	photos = await res.json();
+});
 </script>
 
 <main>
-	<h1>Goodbye {name}!</h1>
+	<h1>What {name} is this?!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
